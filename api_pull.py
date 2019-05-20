670cc89c00cabee6f5f7241e5505aa740c8d9e2a
cooper-mj <coopermj@stanford.edu>
2019-05-20 14:42:18 -0700

    Added dataframe with a, b, c, alpha, beta, gamma, volume features

--- api_pull.py
+++ api_pull.py
@@ -1,24 +1,41 @@
 import json
 import requests
 import pandas as pd
+import numpy as np
 
 materials_codes = open("Raw_dataset/full_mpid_list_cell_size_sort.txt")
 
 url_base = "https://www.materialsproject.org/rest/v1/materials/"
-url_tail = "/vasp/initial_structure?API_KEY="
+url_tail = "/vasp/final_structure?API_KEY="
 api_key = "TsOqaHtJ0LRPqf5jL2Hn"
 
-for code in materials_codes:
-	print(code)
-	material = code.rstrip() #Add a string for it
+features = ["a", "b", "c", "alpha", "beta", "gamma", "volume"]
 
+features_all_elements = []
+
+i = 0
+for code in materials_codes:
+	material = code.rstrip() #Add a string for it
 	url = url_base + material + url_tail + api_key
 	print(url)
 
 	response = requests.get(url)
 	print(response)
 	data = response.json()
-	print(data)
+	
+	material_features = np.zeros(len(features))
+	for j, feature in enumerate(features):
+		material_features[j] = data["response"][0]["final_structure"]["lattice"][feature]
+
+	features_all_elements.append(material_features)
+
+	if i > 5:
+		break
+	i += 1
+print(features_all_elements)
+
+df = pd.DataFrame(data=features_all_elements, columns = features)
+print(df)
 
 
 # Dataframe to csv
