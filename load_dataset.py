7f380396dc314787545b965ff1a700fb2b8b09f4
cooper-mj <coopermj@stanford.edu>
2019-05-25 12:51:17 -0700

    Accuracy metrics added

--- load_dataset.py
+++ load_dataset.py
@@ -1,4 +1,4 @@
-THRESHOLD = 0.2
+THRESHOLD = 0.4
 
 from numpy import genfromtxt
 import numpy as np
@@ -21,12 +21,10 @@ def load_dataset():
 	MPIDs = np.array(total[['MPID']])
 
 	X = np.array(total.iloc[:, 1:48])
-	print(X)
 
 	Y = np.array(total[["min emittance"]])
-	print(Y)
 
-	Y = [1 if y_i < 0.2 else 0 for y_i in Y]
+	Y = [1 if y_i < THRESHOLD else 0 for y_i in Y]
 	return (MPIDs, X, Y)
 
 
@@ -47,4 +45,25 @@ def split_data(tup):
 	MPIDs_test = MPIDs[int(len(MPIDs)*0.8):]
 
 	return (X_train, Y_train, MPIDs_train, X_valid, Y_valid, MPIDs_valid, X_test, Y_test, MPIDs_test)
+
+def accuracy_metric(Y_predictions, Y_actual):
+	true_positives = 0.0
+	true_negatives = 0.0
+	false_positives = 0.0
+	false_negatives = 0.0
+
+	for i, prediction in enumerate(Y_predictions):
+		if Y_actual[i] == 1 and Y_predictions[i] == 1:
+			true_positives += 1
+		if Y_actual[i] == 0 and Y_predictions[i] == 0:
+			true_negatives += 1
+		if Y_actual[i] == 1 and Y_predictions[i] == 0:
+			false_negatives += 1
+		if Y_actual[i] == 0 and Y_predictions[i] == 1:
+			false_positives += 1
+
+	print("Correctly Predicted Proportion : " + str((true_positives + true_negatives) / len(Y_actual)))
+	print("Precision : " + str(true_positives / (true_positives + true_negatives)))
+	print("Recall : " + str(true_positives / (true_positives + false_negatives)))
+
 
