27a3bf62beb58bd937c78f321ef0bba81a80bfb8
theCaiGuy <michaelcai09@gmail.com>
2019-05-24 23:46:46 -0700

    Added new intrinsic emittance files, fixed some errors with labeling script

--- training_labels.py
+++ training_labels.py
@@ -2,9 +2,9 @@ import pandas as pd
 import numpy as np
 import math
 
-NUM_MATERIALS = 3349
+NUM_MATERIALS = 10000
 NUM_MEASUREMENTS = 99 	# excluding first measurement
-NO_EMITTANCE = float128
+NO_EMITTANCE = float('inf')
 
 path = "Raw_dataset/"
 mpid_file = "full_mpid_list_cell_size_sort.txt"
@@ -25,17 +25,24 @@ for i in range(NUM_MATERIALS):
 	print(material)
 	material_info[0] = float(material[3:])
 
-	emittance = np.loadtxt(path + emittance_file + str(i) + ".txt")[1:]
-		# Load txt file and remove first measurement, which is always 0.225
-	np.around(emittance, decimals=1) 	
-		# Place measurements into buckets of nearest 0.1
+	try:
+		emittance = np.loadtxt(path + emittance_file + str(i) + ".txt")[1:]
+	except ValueError:
+		print("Unable to read file " + path + emittance_file + str(i) + ".txt")
+		material_info[1] = NO_EMITTANCE
+		info_all_materials.append(material_info)
+		continue
+
+	# Load txt file and remove first measurement, which is always 0.225
+	np.around(emittance, decimals=1)
+	# Place measurements into buckets of nearest 0.1
 
 	# Maybe there's a more pythonic way to keep only elements that happen twice
 	# in a row, but I sure couldn't find it
 	min_emit = -1
 	for j in range(NUM_MEASUREMENTS - 1):
 		if emittance[j] != 0 and math.isclose(emittance[j], emittance[j+1], rel_tol=0.01):
-			if (min_emit == -1 or emittance[j] < min_emit):			
+			if (min_emit == -1 or emittance[j] < min_emit):
 				min_emit = emittance[j]
 
 	material_info[1] = min_emit if min_emit > 0 else NO_EMITTANCE
