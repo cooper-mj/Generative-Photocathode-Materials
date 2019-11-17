0d70d2d495589a2017a1a1b0f3fa6a531b9b0d2d
JasonZheng20 <jzzheng@stanford.edu>
2019-11-16 19:18:18 -0800

    Tried adam optim

--- GAN/gan.py
+++ GAN/gan.py
@@ -27,10 +27,20 @@ data_stddev = 1.25
 #(name, preprocess, d_input_func) = ("Only 4 moments", lambda data: get_moments(data), lambda x: 4)
 
 # ==============================================================================
-# Data sampler
+# Data sampler for distributions example
 # ==============================================================================
 
 def get_distribution_sampler(mu, sigma):
+    return lambda n: torch.Tensor(np.random.normal(mu, sigma, (1, n)))  # Gaussian
+
+def get_generator_input_sampler():
+    return lambda m, n: torch.rand(m, n)  # Uniform-dist data into generator, _NOT_ Gaussian
+
+# ==============================================================================
+# Data sampler for Particles
+# ==============================================================================
+
+def get_discriminator_sampler(mu, sigma):
     return lambda n: torch.Tensor(np.random.normal(mu, sigma, (1, n)))  # Gaussian
 
 def get_generator_input_sampler():
@@ -69,16 +79,10 @@ def decorate_with_diffs(data, exponent, remove_raw_data=False):
 # ==============================================================================
 # Train loop
 # ==============================================================================
-def train(x, y):
+def get_optimizers():
     discriminator_activation_function = torch.sigmoid
     generator_activation_function = torch.tanh
 
-    dfe, dre, ge = 0, 0, 0
-    d_real_data, d_fake_data, g_fake_data = None, None, None
-    print_interval = 100
-
-    d_sampler = get_distribution_sampler(data_mean, data_stddev)
-    gi_sampler = get_generator_input_sampler()
     G = Generator(input_size=args.g_input_size,
                   hidden_size=args.g_hidden_size,
                   output_size=args.g_output_size,
@@ -87,9 +91,26 @@ def train(x, y):
                       hidden_size=args.d_hidden_size,
                       output_size=args.d_output_size,
                       f=discriminator_activation_function)
+
+    # Choose an optimizer
+    if args.optim == 'Adam':
+        d_optimizer = optim.Adam(D.parameters(), lr=args.d_learning_rate)
+        g_optimizer = optim.Adam(G.parameters(), lr=args.g_learning_rate)
+    else:
+        d_optimizer = optim.SGD(D.parameters(), lr=args.d_learning_rate)
+        g_optimizer = optim.SGD(G.parameters(), lr=args.g_learning_rate, momentum=args.sgd_momentum)
+    return G, D, d_optimizer, g_optimizer
+
+
+def train(x, y):
+    dfe, dre, ge = 0, 0, 0
+    d_real_data, d_fake_data, g_fake_data = None, None, None
+    print_interval = 100
+
+    d_sampler = get_distribution_sampler(data_mean, data_stddev)
+    gi_sampler = get_generator_input_sampler()
     criterion = nn.BCELoss()  # Binary cross entropy: http://pytorch.org/docs/nn.html#bceloss
-    d_optimizer = optim.SGD(D.parameters(), lr=args.d_learning_rate, momentum=args.sgd_momentum)
-    g_optimizer = optim.SGD(G.parameters(), lr=args.g_learning_rate, momentum=args.sgd_momentum)
+    G, D, d_optimizer, g_optimizer = get_optimizers()
 
     for epoch in range(args.num_epochs):
         for d_index in range(args.d_steps):
@@ -129,6 +150,15 @@ def train(x, y):
             print("Epoch %s: D (%s real_err, %s fake_err) G (%s err); Real Dist (%s),  Fake Dist (%s) " %
                   (epoch, dre, dfe, ge, stats(extract(d_real_data)), stats(extract(d_fake_data))))
 
+    print("Plotting the generated distribution...")
+    values = extract(g_fake_data)
+    print(" Values: %s" % (str(values)))
+    plt.hist(values, bins=50)
+    plt.xlabel('Value')
+    plt.ylabel('Count')
+    plt.title('Histogram of Generated Distribution')
+    plt.grid(True)
+    plt.show()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -148,6 +178,8 @@ if __name__ == "__main__":
     parser.add_argument('--print_interval', type=int, default=100)
     parser.add_argument('--d_steps', type=int, default=20)
     parser.add_argument('--g_steps', type=int, default=20)
+
+    parser.add_argument('--optim', type=str, default='SGD')
 
     args = parser.parse_args()
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
