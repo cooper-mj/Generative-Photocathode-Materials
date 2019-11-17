778b4a11e5dcaf20373f20e556f912f8f43330e0
cooper-mj <coopermj@stanford.edu>
2019-11-17 00:58:34 -0800

    Added evaluator NN

---
+++ NN/NeuralNet.py
@@ -0,0 +1,69 @@
+from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import StandardScaler
+
+from load_dataset import load_dataset
+from load_dataset import split_data
+from load_dataset import accuracy_metric
+from load_dataset import augment_data
+
+import numpy as np
+import itertools as it
+from sklearn.model_selection import learning_curve
+import matplotlib.pyplot as plt
+
+# Try excluding 1 element at a time
+def run_experiment(clf, X_train, Y_train, X_valid, Y_valid):
+	num_examples, num_features = np.shape(X_train)
+	num_tests = np.shape(X_valid)[0]
+	# to_exclude = [2, 3, 5, 7, 12, 13, 15]
+	to_exclude = [0, 1, 4, 6, 8, 9, 10, 11, 14]
+	X_train_trunc = np.zeros((num_examples, num_features - 1))
+	X_valid_trunc = np.zeros((num_tests, num_features - 1))
+	# print(X_train)
+	for i in to_exclude:
+		print('Now excluding feature %d' % i)
+		X_train_trunc[:, :i] = X_train[:, :i]
+		X_train_trunc[:, i:] = X_train[:, i + 1:]
+
+		X_valid_trunc[:, :i] = X_valid[:, :i]
+		X_valid_trunc[:, i:] = X_valid[:, i + 1:]
+
+		clf.fit(X_train_trunc, Y_train)
+		Y_valid_predictions = np.zeros(len(Y_valid))
+		for i, example in enumerate(X_valid_trunc):
+			Y_valid_predictions[i] = clf.predict(example.reshape(1, -1))
+
+		accuracy_metric(Y_valid_predictions, Y_valid)
+
+if __name__ == "__main__":
+	# filename = 'material_average_data_plus.csv'
+	# filename = 'combined'
+	filename = 'unit_cell_data_16.csv'
+
+	X_train, Y_train, MPIDs_train, X_valid, Y_valid, MPIDs_valid, X_test, Y_test, MPIDs_test = split_data(load_dataset(filename, 0.2))
+
+	# If data augmentation needed
+	# X_train, Y_train = augment_data(X_train, Y_train, 10)
+
+	print("Training set information:")
+	pos_ex = sum(Y_train)
+	print("Positive examples: " + str(pos_ex))
+	print("Negative examples: " + str(len(Y_train) - pos_ex))
+
+	# print(X_train)
+
+	# scaler = StandardScaler()
+	# scaler.fit(X_train)
+	# X_train = scaler.transform(X_train)
+	# X_valid = scaler.transform(X_valid)
+	# X_test = scaler.transform(X_test)
+
+	clf = MLPClassifier(hidden_layer_sizes=(64, 128, 256, 512, 256, 128, 64), alpha=0.9, tol=0.001, learning_rate_init=0.0008, max_iter=400, n_iter_no_change=10, verbose=True).fit(X_train, Y_train)
+	# clf = MLPClassifier(hidden_layer_sizes=(64, 128, 64), learning_rate_init=0.0008, max_iter=400, n_iter_no_change=10, verbose=True).fit(X_train, Y_train)
+	Y_valid_predictions = np.zeros(len(Y_valid))
+	for i, example in enumerate(X_valid):
+		Y_valid_predictions[i] = clf.predict(example.reshape(1, -1))
+
+	clf = MLPClassifier(hidden_layer_sizes=(128, 256, 512, 256, 128, 64), learning_rate_init = 0.0008, max_iter=300, n_iter_no_change=15, verbose=True) #.fit(X_train, Y_train)
+	accuracy_metric(Y_valid_predictions, Y_valid)
+	
---
+++ NN/load_dataset.py
@@ -0,0 +1,148 @@
+from numpy import genfromtxt
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+
+''' load_dataset: Loads and merges cleaned-up data
+	@param threshold: emittance threshold below which a data point is considered positive example
+		Set threshold = -1 for non-binary models
+	@return tuple of numpy arrays for MPIDs, features, and labels
+'''
+def load_dataset(filename, threshold=0.2):
+	# Get data
+	Y_full = pd.read_csv('emittance_labels.csv')
+	X_full = None
+	if filename == "combined":
+		X_unit_cell = pd.read_csv('unit_cell_data_16.csv')
+		# X_avg = pd.read_csv('material_average_data.csv')
+		X_avg = pd.read_csv('material_average_data_plus.csv')
+		X_full = pd.merge(X_unit_cell, X_avg, on='MPID')
+	else:
+		X_full = pd.read_csv(filename)
+
+	total = pd.merge(X_full, Y_full, on="MPID")
+
+	# Random state below is a seed - change this when we go to run for real
+	total = np.array(total.sample(frac=1, random_state=229).reset_index(drop=True))
+	total = np.array([total[i] for i in range(len(total)) if total[i, -1] != float('inf')])
+
+	MPIDs = np.array(total[:, 0])
+	X = np.array(total[:, 1:-1])
+
+	# Replace NaN/-1/0's with average col value as needed
+	nan_locs = np.isnan(X)
+	X[nan_locs] = -1
+	# print(len(X[0]))
+	# print(X)
+	_, colnum = X.shape
+
+	nonexistent = -1
+	if filename == 'material_average_data_plus.csv':
+		nonexistent = 0
+
+	for col in range(colnum):
+		adj_col = X[:, col]
+		mask = adj_col != nonexistent
+		mean = np.mean(adj_col * mask)
+		adj_col[adj_col == nonexistent] = mean
+		X[:, col] = adj_col
+
+	# if filename == 'material_average_data.csv' or 'combined':
+	# 	scaler = StandardScaler()
+	# 	scaler.fit(X[-9:])
+	# 	X[-9:] = scaler.transform(X[-9:])
+
+	# Scale data
+	# if filename == 'material_average_data.csv' or 'combined':
+	# 	scaler = StandardScaler()
+	# 	scaler.fit(X[-8:]) # scale everything except MPID and atomic number
+	# 	X = scaler.transform(X)
+
+	# if filename == 'material_average_data_plus.csv':
+	# 	scaler = StandardScaler()
+	# 	scaler.fit(X[-16:]) # scale everything except MPID
+	# 	X = scaler.transform(X)
+
+
+	Y = np.array(total[:, -1])
+
+	if threshold != -1:
+		Y = [1 if y_i <= threshold else 0 for y_i in Y]
+
+	return (MPIDs, X, Y)
+
+''' load_dataset: Loads and merges cleaned-up data
+	@param tup: tuple of MPIs, X's, Y's as returned by load_dataset
+	@return tuple of numpy arrays for training, validation, and test sets
+'''
+def split_data(tup, train_split = 0.8, valid_split = 0.1, test_split = 0.1):
+
+	MPIDs, X, Y = tup
+
+	assert (train_split + valid_split + test_split == 1),"The proportion of data dedicated to train, validation, and test sets does not sum to 1."
+
+	training_threshold = train_split
+	valid_threshold = train_split + valid_split
+
+	X_train = X[:int(len(X)*training_threshold)]
+	Y_train = Y[:int(len(Y)*training_threshold)]
+	MPIDs_train = MPIDs[:int(len(MPIDs)*training_threshold)]
+
+	X_valid = X[int(len(X)*training_threshold):int(len(X)*valid_threshold)]
+	Y_valid = Y[int(len(Y)*training_threshold):int(len(X)*valid_threshold)]
+	MPIDs_valid = MPIDs[int(len(MPIDs)*training_threshold):int(len(X)*valid_threshold)]
+
+	X_test = X[int(len(X)*valid_threshold):]
+	Y_test = Y[int(len(Y)*valid_threshold):]
+	MPIDs_test = MPIDs[int(len(MPIDs)*valid_threshold):]
+
+	return (X_train, Y_train, MPIDs_train, X_valid, Y_valid, MPIDs_valid, X_test, Y_test, MPIDs_test)
+
+def accuracy_metric(Y_predictions, Y_actual):
+	true_positives = 0.0
+	true_negatives = 0.0
+	false_positives = 0.0
+	false_negatives = 0.0
+
+	for i, prediction in enumerate(Y_predictions):
+		if Y_actual[i] == 1 and Y_predictions[i] == 1:
+			true_positives += 1
+		if Y_actual[i] == 0 and Y_predictions[i] == 0:
+			true_negatives += 1
+		if Y_actual[i] == 1 and Y_predictions[i] == 0:
+			false_negatives += 1
+		if Y_actual[i] == 0 and Y_predictions[i] == 1:
+			false_positives += 1
+
+	accuracy = (true_positives + true_negatives) / len(Y_actual)
+	precision = true_positives / (true_positives + true_negatives)
+	recall = true_positives / (true_positives + false_negatives)
+	F1 = 2 * (precision * recall) / (precision + recall)
+
+	print("Correctly Predicted Proportion : " + str(accuracy))
+	print("Precision : " + str(precision))
+	print("Recall : " + str(recall))
+	print("F1 : " + str(F1))
+
+
+def augment_data(X, Y, num_permutations):
+	atoms = X[:, -64:]
+	# print(X)
+	# atoms = X[:, -6:]
+	XT = atoms.T
+	m, n = np.shape(XT)[0] // 2, np.shape(XT)[1]
+	all_new_inputs = None
+	all_labels = None
+
+	for i in range(num_permutations):
+		perm = XT.reshape(m, -1, n)[np.random.permutation(m)].reshape(-1,n)
+		new_data = np.concatenate((X[:, :-64], perm.T), axis=1)
+		# print(new_data)
+		if i == 0:
+			all_new_inputs = new_data
+			all_labels = Y
+		else:
+			# print('Concatenating!')
+			all_new_inputs = np.concatenate((all_new_inputs, new_data), axis=0)
+			all_labels = np.concatenate((all_labels, Y), axis=0)
+	return (np.concatenate((X, all_new_inputs), axis=0), np.concatenate((Y, all_labels), axis=0))
